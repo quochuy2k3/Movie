925c73b88e7ad83a30b05136993b772e0aeab12f
quochuy2k3 <voquochuy3006@gmail.com>
2023-05-22 20:17:45 +0700

    1st

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Movies from './component'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Movies api = "http://localhost:8080/test"/>
+  )
+}
+
+export default App
